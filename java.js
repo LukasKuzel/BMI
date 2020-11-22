06ddd4c9ad061f8d3bab3415141db18a64c1f6c6
Lukas <67201471+LukasKuzel@users.noreply.github.com>
2020-11-22 23:04:39 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,13 +1,13 @@
 function BMI() {
-    var h=document.getElementById('h').value;
-    var w=document.getElementById('w').value;
-    var bmi=w/(h/100*h/100);
-    var bmio=(bmi.toFixed(2));
+    let h=document.getElementById('h').value;
+    let w=document.getElementById('w').value;
+    let bmi=w/(h/100*h/100);
+    let bmio=(bmi.toFixed(2));
 
     document.getElementById("result").innerHTML="Your BMI is " + bmio;
 }
 
-var audio = new Audio('music/aerohead-path-of-the-fireflies.mp3');
+let audio = new Audio('music/aerohead-path-of-the-fireflies.mp3');
 function play1(){
     audio.volume = 0.03
     audio.play();
